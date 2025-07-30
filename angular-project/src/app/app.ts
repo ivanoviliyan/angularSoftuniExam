193c29564c3f4fecaf31493070f1bf53dd622dfd
Iliyan Ivanov <ivanov.iliyan_mail@yahoo.com>
2025-07-30 18:13:09 +0300

    adjusting html and adding componets

--- angular-project/src/app/app.ts
+++ angular-project/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navigation } from './shared/navigation/navigation';
 import { Footer } from './shared/footer/footer';
+import { Home } from './pages/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navigation, Footer],
+  imports: [RouterOutlet, Navigation, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
