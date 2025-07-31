1ca2f00278f8934ae6d77dd05487b1cd24cf6ce2
Iliyan Ivanov <ivanov.iliyan_mail@yahoo.com>
2025-07-31 12:46:05 +0300

    adding html and css in the project pages

--- angular-project/src/app/app.ts
+++ angular-project/src/app/app.ts
@@ -3,12 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { Navigation } from './shared/navigation/navigation';
 import { Footer } from './shared/footer/footer';
 import { Home } from './pages/home/home';
+import { Tvshows } from './pages/tvshows/tvshows';
+import { Movies } from './pages/movies/movies';
+import { Mylist } from './pages/mylist/mylist';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navigation, Footer, Home],
+  imports: [RouterOutlet, Navigation, Footer, Home, Tvshows, Movies, Mylist],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular-project');
