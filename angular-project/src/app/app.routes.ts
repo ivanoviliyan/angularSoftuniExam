66ce7970efdf4a1f023a9d8155a90ddae7527a05
Iliyan Ivanov <ivanov.iliyan_mail@yahoo.com>
2025-08-03 13:42:04 +0300

    creating routes

--- angular-project/src/app/app.routes.ts
+++ angular-project/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { About } from './pages/about/about';
+import { Home } from './pages/home/home';
+import { Managemovies } from './pages/managemovies/managemovies';
+import { Movies } from './pages/movies/movies';
+import { Mylist } from './pages/mylist/mylist';
+import { Register } from './pages/register/register';
+import { Singin } from './pages/singin/singin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'manage-movies', component: Managemovies },
+  { path: 'movies', component: Movies },
+  { path: 'my-list', component: Mylist },
+  { path: 'register', component: Register },
+  { path: 'sign-in', component: Singin }, 
+];
--- angular-project/src/app/app.ts
+++ angular-project/src/app/app.ts
@@ -2,27 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navigation } from './shared/navigation/navigation';
 import { Footer } from './shared/footer/footer';
-import { Home } from './pages/home/home';
-import { Movies } from './pages/movies/movies';
-import { Mylist } from './pages/mylist/mylist';
-import { Singin } from './pages/singin/singin';
-import { Register } from './pages/register/register';
-import { About } from './pages/about/about';
-import { Managemovies } from './pages/managemovies/managemovies';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     Navigation,
-    Footer,
-    Home,
-    Movies,
-    Mylist,
-    Singin,
-    Register,
-    About,
-    Managemovies
+    Footer
   ],
   templateUrl: './app.html',
   styleUrl: './app.css',
