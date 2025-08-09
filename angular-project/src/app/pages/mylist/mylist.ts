de8c8657a64e3d22e90bdf594fdeec2ad2cdd39a
Iliyan Ivanov <ivanov.iliyan_mail@yahoo.com>
2025-08-09 10:09:22 +0300

    adding remove from list feature for mylist page

--- angular-project/src/app/pages/mylist/mylist.ts
+++ angular-project/src/app/pages/mylist/mylist.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { MovieService } from '../managemovies/managemovie.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../../auth.service';
 import { forkJoin } from 'rxjs';
@@ -15,10 +15,10 @@ interface Movie {
   duration: string;
   rating?: number;
   desc: string;
-  watched: string[]; // userId-та, които са гледали филма
+  watched: string[];
   _ownerId: string;
   _movieId: string;
-  isWatched?: boolean; // за UI затъмняване
+  isWatched?: boolean;
 }
 
 @Component({
@@ -51,16 +51,12 @@ export class Mylist implements OnInit {
     this.http.get<Movie[]>(this.watchLaterUrl).subscribe({
       next: (watchLaterMovies) => {
         const filtered = watchLaterMovies.filter((m) => m._ownerId === ownerId);
-        // Масив от Observable, които зареждат допълнителната информация
         const detailedRequests = filtered.map((movie) =>
           this.http.get<Movie>(
             `http://localhost:3030/data/ng-movies/${movie._movieId}`
           )
         );
 
-        console.log(detailedRequests)
-
-        // Изпълняваме всички заяви едновременно
         forkJoin(detailedRequests).subscribe({
           next: (detailedMovies) => {
             this.movies = detailedMovies.map((movie) => ({
@@ -96,7 +92,6 @@ export class Mylist implements OnInit {
           : [];
 
         if (currentWatched.includes(this.currentUserId)) {
-          // Вече е маркиран като гледан, не правим нищо
           return;
         }
 
@@ -113,7 +108,6 @@ export class Mylist implements OnInit {
           )
           .subscribe({
             next: () => {
-              // Обновяваме локалния обект
               movie.isWatched = true;
               movie.watched = updatedWatched;
               this.chr.detectChanges();
@@ -126,6 +120,70 @@ export class Mylist implements OnInit {
       error: (error) => {
         console.error('Error fetching movie data:', error);
       },
+    });
+  }
+
+  removeFromList(movie: Movie): void {
+    const getUrl = `${this.watchLaterUrl}?where=_movieId%3D%22${movie._id}%22`;
+
+    this.http.get<Movie[]>(getUrl).subscribe({
+      next: (response) => {
+        if (response.length > 0) {
+          const id = response[0]._id;
+          const ownerId = response[0]._ownerId;
+          const deleteUrl = `${this.watchLaterUrl}/${id}`;
+          const patchUrl = `http://localhost:3030/data/ng-movies?where=_ownerId%3D%22${ownerId}%22`;
+
+          this.http.get<any[]>(patchUrl).subscribe({
+            next: (movies) => {
+              movies.forEach((m) => {
+                if (Array.isArray(m.watched) && m.watched.includes(ownerId)) {
+                  const updatedWatched: string[] = (
+                    m.watched as string[]
+                  ).filter((uid: string) => uid !== ownerId);
+                  const updateUrl = `http://localhost:3030/data/ng-movies/${m._id}`;
+
+                  this.http
+                    .patch(
+                      updateUrl,
+                      { watched: updatedWatched },
+                      {
+                        headers: this.movieService.credits(),
+                      }
+                    )
+                    .subscribe({
+                      next: () =>
+                        console.log(
+                          `Removed ${ownerId} from watched in movie ${m._id}`
+                        ),
+                      error: (err) =>
+                        console.error(`Error updating movie ${m._id}:`, err),
+                    });
+                }
+              });
+            },
+            error: (err) =>
+              console.error('Error fetching movies for patch:', err),
+          });
+
+          this.http
+            .delete(deleteUrl, {
+              headers: this.movieService.credits(),
+              observe: 'response',
+            })
+            .subscribe({
+              next: () => {
+                this.movies = this.movies.filter((m) => m._id !== movie._id);
+                this.chr.detectChanges();
+                console.log(`Movie ${movie._id} removed successfully.`);
+              },
+              error: (error) => console.error('Delete error:', error),
+            });
+        } else {
+          console.log('No movie found to remove.');
+        }
+      },
+      error: (error) => console.error('Get error:', error),
     });
   }
 }
